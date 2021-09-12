a9fc99bffd8b566a1a780a364d9355bbde13b641
iagardi <garbigz@gmail.com>
2021-09-12 14:28:47 +0100

    number buttons work -- some functionality added to operators -- clear button works

--- README.md
+++ README.md
@@ -12,6 +12,13 @@ A calculator using HTML, SCSS and JS
 #2 Commit:
 
 - main container size and layout -> flex column
+- main layout finished, onto JS now
+
+#3 Commit:
+
+- number buttons functionality added
+- some functionality added to basic operators (move number to top display, show operator)
+- Clear button functionality added
 
 ## Project plan:
 
@@ -19,6 +26,7 @@ A calculator using HTML, SCSS and JS
    - mixture of flex and grid base
    - fonts and colors in variables
 2. Add initial button functions using JS
+   - 6 classes for buttons: numbers, operators, float,
    - Buttons value to show on display
 3. Add functioning operators
    - Need a lot of research by looking for specific functions rather than calculator specific answers
---
+++ main.js
@@ -0,0 +1,49 @@
+// Declare variables for buttons and display
+const botDisplay = document.querySelector(".display-bot");
+const topDisplay = document.querySelector(".display-top");
+
+const numbers = document.querySelectorAll(".btn--num");
+const basicOps = document.querySelectorAll(".btn--ops");
+const clear = document.querySelector(".btn--clear");
+
+let operator = "";
+let topNum = 0;
+
+// Buttons to push numbers to bot display
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    const value = number.innerHTML;
+    const length = botDisplay.innerHTML.length;
+    console.log(value); // testing during progress
+
+    if (botDisplay.innerHTML == 0) {
+      botDisplay.innerHTML = value;
+    } else if (length < 8) {
+      botDisplay.innerHTML += value;
+    }
+  });
+});
+
+// Clear button
+clear.addEventListener("click", (event) => {
+  // Reset both displays
+  topDisplay.innerHTML = "0";
+  botDisplay.innerHTML = "0";
+  // Reset global variables
+  operator = "";
+  topNum = 0;
+});
+
+// Basic operators -> accept entry and move value + operator to top display
+basicOps.forEach((basicop) => {
+  basicop.addEventListener("click", (event) => {
+    // Record current operator into global variable
+    const currentOp = basicop.innerHTML;
+    operator = currentOp;
+
+    const firstNum = botDisplay.innerHTML;
+    console.log(firstNum);
+    topDisplay.innerHTML = firstNum + currentOp;
+    botDisplay.innerHTML = "0";
+  });
+});
